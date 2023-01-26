7301ae066151ab5a9ff7497cbb17098697bdb731
Artyom Danilov <defytheflow@gmail.com>
2023-01-26 19:35:35 +0300

    fix: handle space key on disabled button

--- src/button.tsx
+++ src/button.tsx
@@ -3,18 +3,20 @@ import React from "react";
 export function Button({
   disabled,
   onClick,
+  onKeyDown,
   ...rest
 }: React.ComponentPropsWithoutRef<"button">) {
   return (
     <button
       {...rest}
-      aria-disabled={disabled}
-      onClick={disabled ? e => e.preventDefault() : onClick}
       style={{
         ...rest.style,
         pointerEvents: disabled ? "none" : "all",
         opacity: disabled ? 0.5 : undefined,
       }}
+      aria-disabled={disabled || undefined}
+      onClick={disabled ? e => e.preventDefault() : onClick}
+      onKeyDown={disabled ? e => (e.key === " " ? e.preventDefault() : null) : onKeyDown}
     />
   );
 }
